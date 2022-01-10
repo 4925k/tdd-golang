a6112d68f5d098eaecdeda5e049eadc2506782e2
dp <dibek.poudyal@vairav.net>
2022-01-10 15:49:19 +0545

    testing hello world

--- helloworld/main_test.go
+++ helloworld/main_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	check := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		check(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is passed", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		check(t, got, want)
+	})
 }
